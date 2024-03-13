8f17f1901dcf7d539fd510d3d515f9468269714f
Oleksandr Moskalyk <alexundercover191@gmail.com>
2024-03-13 17:08:01 +0200

    Add sound to drone

---
+++ Assets/Scripts/DroneSoundController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using UnityEngine;
+
+public class DroneSoundController : MonoBehaviour
+{
+    [SerializeField] private float changePitchAmount = 1.5f;
+    [SerializeField] private Rigidbody droneRigidBody;
+
+    private float defaultPitch;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        defaultPitch = audioSource.pitch;
+    }
+
+    private void Update()
+    {
+        // Calculate the velocity magnitude of the rigidbody
+        float velocityMagnitude = droneRigidBody.velocity.magnitude;
+
+        // Calculate the target pitch based on the velocity magnitude
+        float targetPitch = velocityMagnitude > 0.5f ? defaultPitch * changePitchAmount : defaultPitch;
+
+        // Smoothly change the pitch of the audio source
+        float currentPitch = audioSource.pitch;
+        float pitchChangeSpeed = 1f; // Adjust this value to control the speed of pitch change
+
+        // Smoothly change the pitch of the audio source
+        audioSource.pitch = Mathf.Lerp(currentPitch, targetPitch, Time.deltaTime * pitchChangeSpeed);
+    }
+}
