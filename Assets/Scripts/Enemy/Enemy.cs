2fe77dcd4a130fa7428b026159ae300996bcab6a
Oleksandr Moskalyk <alexundercover191@gmail.com>
2024-04-03 17:47:47 +0300

    Reset enemy on episode begin

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,5 +3,20 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    private Rigidbody rigidBody;
 
+    private void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    public void ResetEnemy()
+    {
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = Vector3.zero;
+
+        // Randomize enemy position
+        float randomX = UnityEngine.Random.Range(-1f, 64);
+        transform.localPosition = new Vector3(randomX, 2.25f, 51f);
+    }
 }
